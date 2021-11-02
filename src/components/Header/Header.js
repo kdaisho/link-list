05427da0e616628ea8a5769ab8db849210844c1e
Daisho <daishokomiyama@gmail.com>
2021-11-02 00:42:20 -0400

    Create a prototype

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { header, title } from './header.module.css'
+
+const Header = () => {
+  return (
+    <header className={header}>
+      <h1 className={title}>Link List</h1>
+    </header>
+  )
+}
+
+export default Header
